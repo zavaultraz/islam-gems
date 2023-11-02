0dbc13bdac64554da9b6a40fa4d849ea3ce9a972
zavaultraz <zavazaneta@gmail.com>
2023-11-02 13:01:55 +0700

    islamic-gems

--- script.js
+++ script.js
@@ -89,3 +89,12 @@ fetch(jadwalAPI)
 
         });
     })
+
+    //jam
+    setInterval(()=>{
+        let currentTime = new Date();
+    
+        hrs.innerHTML = (currentTime.getHours()<10?"0":"") + currentTime.getHours();
+        min.innerHTML = (currentTime.getMinutes()<10?"0":"") + currentTime.getMinutes();
+        sec.innerHTML = (currentTime.getSeconds()<10?"0":"") + currentTime.getSeconds();
+    },1000)
